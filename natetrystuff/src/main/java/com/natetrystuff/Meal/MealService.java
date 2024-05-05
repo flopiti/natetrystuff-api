beda49f833e013ae90696d6bafdbbfab895b9a98
Nathan <npieraut@live.fr>
2024-05-05 02:01:50 -0400

    check if any meal-schedule exist with it first

--- natetrystuff/src/main/java/com/natetrystuff/Meal/MealService.java
+++ natetrystuff/src/main/java/com/natetrystuff/Meal/MealService.java
@@ -2,15 +2,21 @@ package com.natetrystuff.Meal;
 
 import org.springframework.stereotype.Service;
 
+import com.natetrystuff.MealSchedule.MealSchedule;
+import com.natetrystuff.MealSchedule.MealScheduleService;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MealService {
 
     private final MealRepository mealRepository;
+    private final MealScheduleService mealScheduleService;
 
-    public MealService(MealRepository mealRepository) {
+    public MealService(MealRepository mealRepository, MealScheduleService mealScheduleService) {
         this.mealRepository = mealRepository;
+        this.mealScheduleService = mealScheduleService;
     }
 
     public List<Meal> listAllMeals() {
@@ -36,6 +42,13 @@ public class MealService {
     }
 
     public void delete(Long id) {
+        List<MealSchedule> schedules = mealScheduleService.listAllSchedules().stream()
+        .filter(meal -> meal.getMeal().getMealId().equals(id)).collect(Collectors.toList());
+
+        mealScheduleService.listAllSchedules().stream()
+            .filter(meal -> meal.getMeal().getMealId().equals(id))
+            .forEach(meal -> mealScheduleService.deleteSchedule(meal.getScheduleId()));
+    
         mealRepository.deleteById(id);
     }
 }
