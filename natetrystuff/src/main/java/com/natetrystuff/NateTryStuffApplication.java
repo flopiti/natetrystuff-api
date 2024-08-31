45af0e6f38da5479874f789c9c58e8eeca502af6
Nathan <npieraut@live.fr>
2024-08-31 16:02:06 -0400

    I think I don't need that

--- natetrystuff/src/main/java/com/natetrystuff/NateTryStuffApplication.java
+++ natetrystuff/src/main/java/com/natetrystuff/NateTryStuffApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.natetrystuff.Meal, package com.natetrystuff,  com.natetrystuff.MealSchedule"})
 public class NateTryStuffApplication {
 
 	public static void main(String[] args) {
